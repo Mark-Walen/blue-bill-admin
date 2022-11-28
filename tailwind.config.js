342ecc274688aa0bbd7fb2e01857459d6fe9a230
Mark-Walen <walen.mark2509758@gmail.com>
2022-11-28 11:36:36 +0800

    add tailwindcss related config js file.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    spacing: {
+      sm: '8px',
+      md: '16px',
+      lg: '24px',
+      xl: '48px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
