eecbcbc2f208a630e907d497e5fa1558d64cc506
Mark-Walen <walen.mark2509758@gmail.com>
2022-12-02 00:50:01 +0800

    success to config element-plus auto import with unplugin-vue-components

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from "path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {createSvgIconsPlugin} from "vite-plugin-svg-icons";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +17,15 @@ export default defineConfig({
             iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
             symbolId: 'bm-icon-[name]',
             customDomId: 'bm-svg_icons'
-        })
+        }),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+            dts: path.resolve(process.cwd(), 'src/auto-imports.d.ts')
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+            dts: path.resolve(process.cwd(), 'src/components.d.ts')
+        }),
     ],
     resolve: {
         alias: {
