1896cbaaf6fb23bd1defd625791ffac873a00fd1
Mark-Walen <mark_walen@qq.com>
2023-09-02 21:16:13 +0800

    update: separate for async load.

--- src/views/BmMain.vue
+++ src/views/BmMain.vue
@@ -1,11 +1,10 @@
 <template>
-$END$
+    <el-main :style="{'--el-main-padding': !$bm.screen.gt.xs ?'0px': '24px'}">
+        <router-view />
+    </el-main>
 </template>
 
-<script>
-export default {
-name: "BmMain"
-}
+<script setup>
 </script>
 
 <style scoped>
