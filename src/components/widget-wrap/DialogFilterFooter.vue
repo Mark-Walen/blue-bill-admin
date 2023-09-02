611fe29f966cfa8b8ed84dd81f7d11948d31c939
Mark-Walen <mark_walen@qq.com>
2023-09-02 21:20:35 +0800

    feat: dialog filter footer.

--- src/components/widget-wrap/DialogFilterFooter.vue
+++ src/components/widget-wrap/DialogFilterFooter.vue
@@ -1,13 +1,48 @@
 <template>
-$END$
+    <bm-card class="bm-filter-controller">
+        <bm-card class="reset">
+            <el-button>重置</el-button>
+        </bm-card>
+        <bm-card class="done">
+            <el-button @click="done">完成</el-button>
+        </bm-card>
+    </bm-card>
 </template>
 
 <script>
-export default {
-name: "DialogFilterFooter"
-}
+import { defineComponent, getCurrentInstance, nextTick } from "vue";
+import BmCard from "../card/BmCard";
+
+export default defineComponent({
+    name: "DialogFilterFooter",
+    components: {
+        BmCard
+    },
+    setup(){
+        const { emit } = getCurrentInstance()
+        const done = () => {
+            // console.log(124);
+            nextTick(() => {
+                emit("done", 123);
+            });
+        }
+        return {
+            done
+        }
+    }
+});
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+.bm-filter-controller
+    display flex
+    justify-content flex-end
+
+    .reset
+    .done
+        margin 8px 8px
+    .done
+        margin-right 12px
+
 
 </style>
