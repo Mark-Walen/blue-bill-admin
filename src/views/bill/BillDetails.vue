259d05c8189a1129a16b39083023342387fe6ef2
Mark-Walen <mark_walen@qq.com>
2023-09-07 01:13:25 +0800

    refactor: rollback el-table-v2 to el-table and ready.

--- src/views/bill/BillDetails.vue
+++ src/views/bill/BillDetails.vue
@@ -1,34 +1,38 @@
 <template>
-    <bm-card class="detail-container flex flex-col">
+    <bm-card class="detail-container flex flex-col" ref="detailContainerRef">
         <bm-card class="content-toolbar">
             <collapse-transition>
                 <panel-filter :options="filterOptions" :alwaysExpanded="false"></panel-filter>
             </collapse-transition>
         </bm-card>
-        <bm-card class="table-container flex-1 p-lg pt-2">
-            <el-auto-resizer>
-                <template #default="{ height, width }">
-                    <el-table-v2
-                        :width="width"
-                        :height="height"
-                        :columns="columns"
-                        :data="billItemData"
-                        table-layout="auto"
-                        :sort-by="{prop: 'paymentTime', order: 'descending'}">
-                        <template #empty>
-                            <div class="flex items-center justify-center">
-                                <el-empty description="还没有账单数据哦， 快来添加吧！"/>
-                            </div>
-                        </template>
-                    </el-table-v2>
+        <bm-card class="table-container flex-1">
+            <el-table ref="billDetailTableRef"
+                      :data="billItemData"
+                      :highlight-current-row="highlightCurrentRow"
+                      :max-height="tableMaxHeight"
+                      @row-click="handleCurrentRowClick"
+                      style="width: 100%;"
+                      table-layout="auto">
+                <el-table-column type="selection" :width="48" />
+                <el-table-column prop="name" label="商品名称" width="96" />
+                <el-table-column prop="money" label="金额 ￥" max-width="128"/>
+                <el-table-column prop="payee" label="商家/收款方"
+
+                                 :show-overflow-tooltip="{ enterable: true, placement: 'top', showArrow: true, hideAfter: 200, popperOptions: { strategy: 'fixed' } }" />
+                <el-table-column prop="paymentTime" label="交易时间" :width="200"/>
+                <el-table-column prop="memo" label="备注"/>
+                <template #empty>
+                    <div class="flex items-center justify-center">
+                        <el-empty description="还没有账单数据哦， 快来添加吧！" />
+                    </div>
                 </template>
-            </el-auto-resizer>
+            </el-table>
         </bm-card>
     </bm-card>
 </template>
 
 <script>
-import { computed, defineComponent, h, ref } from "vue";
+import { computed, defineComponent, onMounted, ref } from "vue";
 import i18n from "../../i18n";
 import BmCard from "../../components/card/BmCard";
 import en from "element-plus/dist/locale/en.mjs";
@@ -39,82 +43,50 @@ import CollapseTransition from "@/components/collapse-transition/collapse-transi
 export default defineComponent({
     components: { CollapseTransition, PanelFilter, BmCard },
     setup() {
-        const currentPage = ref(1);
-        const pageSize = ref(18);
+        const tableMaxHeight = ref('600')
+        const clientHeight = ref('')
+        const highlightCurrentRow = ref(false);
         const billItemData = ref([]);
-        const total = ref(0);
         const lang = ref(i18n.global.locale);
         const locale = computed(() => (lang.value === "zh-cn" ? zhCn : en));
-        const columns = [
-            {
-                key: "id",
-                dataKey: "id",
-                title: "ID",
-                minWidth: 88
-            },
-            {
-                key: "name",
-                dataKey: "name",
-                title: "商品名称",
-                minWidth: 184
-            },
-            {
-                key: "money",
-                dataKey: "money",
-                title: "金额（￥）",
-                minWidth: 120,
-                sortable: true,
-                cellRenderer: ({ cellData }) => h(
-                    "span",
-                    {},
-                    { default: () => Number(cellData).toFixed(2) }
-                )
-            },
-            {
-                key: "paymentTime",
-                dataKey: "paymentTime",
-                title: "交易时间",
-                minWidth: 192,
-                sortable: true
-            },
-            {
-                key: "payee",
-                dataKey: "payee",
-                title: "商户名称 / 收款方",
-                minWidth: 216
-            },
-            {
-                key: "memo",
-                dataKey: "memo",
-                title: "备注",
-                minWidth: 200
-            }
-        ];
+        const currentRow = ref();
+        const billDetailTableRef = ref(null);
+        const detailContainerRef = ref(null);
+
         billItemData.value = [
             {
-              id: 1,
-              name: "食盐",
-              money: "2",
-              billSno: "4200057830202211153414005616",
-              payee: "家里购连锁生活超市",
-              paymentTime: "2022年11月15日 08:28:38",
-              via: "微信零钱",
-              memo: "偷吃盐"
+                id: 1,
+                name: "食盐",
+                money: "2.00",
+                billSno: "4200057830202211153414005616",
+                payee: "家里购连锁生活超市",
+                paymentTime: "2022年11月15日 08:28:38",
+                via: "微信零钱",
+                memo: "偷吃盐"
             },
             {
-              id: 2,
-              name: "30元话费",
-              money: "30",
-              billSno: "4200057830202211153414005070",
-              payee: "中国移动金融科技有限公司",
-              paymentTime: "2022年11月14日 11:19:29",
-              via: "建设银行储蓄卡(9465)",
-              memo: "可恶的中国移动"
+                id: 2,
+                name: "30元话费",
+                money: "3000000000.00",
+                billSno: "4200057830202211153414005070",
+                payee: "中国移动金融科技有限公司",
+                paymentTime: "2022年11月14日 11:19:29",
+                via: "建设银行储蓄卡(9465)",
+                memo: "可恶的中国移动"
             }
         ];
-        const detailContainer = {
-            height: "100%" //48 * (billItemData.value.length + 3) + "px"
-        };
+        for (let i = 0; i < 100; i++) {
+            billItemData.value.push({
+                id: 2,
+                name: "30元话费",
+                money: "3000000000.00",
+                billSno: "4200057830202211153414005070",
+                payee: "中国移动金融科技有限公司",
+                paymentTime: "2022年11月14日 11:19:29",
+                via: "建设银行储蓄卡(9465)",
+                memo: "可恶的中国移动"
+            })
+        }
         const filterOptions = [{
             field: "ear_tag_search",
             type: "input",
@@ -126,36 +98,34 @@ export default defineComponent({
             label: "牛舍号",
             placeholder: "按牛舍号搜索"
         }];
-        const showBillItem = (id) => {
-            console.log(id);
-        };
-        const editBillItem = (id) => {
-            console.log(id);
-        };
-        const deleteBillItem = (id) => {
-            console.log(id);
-        };
-        const handleSizeChange = (size) => {
-            console.log(pageSize.value);
-        };
-        const handleCurrentChange = (page) => {
-            console.log(currentPage.value);
+
+        onMounted(() => {
+            clientHeight.value = `${document.documentElement.clientHeight}` //获取浏览器可视区域高度
+            tableMaxHeight.value = clientHeight.value - 96
+            window.onresize = function () {
+                clientHeight.value = `${document.documentElement.clientHeight}` //获取浏览器可视区域高度
+                tableMaxHeight.value = clientHeight.value - 96
+            }
+        })
+
+        const handleCurrentRowClick = (val) => {
+            if (currentRow.value !== val) {
+                currentRow.value = val
+                highlightCurrentRow.value = true
+            } else {
+                highlightCurrentRow.value = !highlightCurrentRow.value;
+            }
         };
         return {
-            currentPage,
-            pageSize,
-            columns,
+            highlightCurrentRow,
             billItemData,
-            total,
+            billDetailTableRef,
+            detailContainerRef,
+            tableMaxHeight,
             lang,
             locale,
             filterOptions,
-            detailContainer,
-            showBillItem,
-            editBillItem,
-            deleteBillItem,
-            handleSizeChange,
-            handleCurrentChange
+            handleCurrentRowClick
         };
     }
 });
@@ -165,8 +135,7 @@ export default defineComponent({
 .detail-container
     display flex
     flex-flow column
-    height: v-bind('detailContainer.height')
-    max-height 100%
+    height 100%
 
 .operation-group
     width 100%
