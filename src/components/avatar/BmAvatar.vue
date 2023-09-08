83916bd1c1081a874a5fc0755a3b7bcebfac8cff
Mark-Walen <mark_walen@qq.com>
2023-09-08 08:49:12 +0800

    fix: public image url error

---
+++ src/components/avatar/BmAvatar.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-avatar :src="getImageUrl(src)" :size="size" />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { getImageUrl } from "@/utils/util";
+
+export default defineComponent({
+    name: 'BmAvatar',
+    props: {
+        src: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: [Number, String],
+            validator: (value) => {
+                if (typeof value === 'number' && !isNaN(value)) return true
+                return [ 'small', 'default', 'large' ].includes(value)
+            }
+        }
+    },
+    methods: {
+        getImageUrl
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/img/BmImage.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-image :src="getImageUrl(src)" :loading="loading" />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { getImageUrl } from "@/utils/util";
+
+export default defineComponent({
+    name: 'BmImage',
+    props: {
+        src: {
+            type: String,
+            required: true
+        },
+        loading: {
+            type: String,
+            validator: (value) => {
+                return [ 'eager', 'lazy' ].includes(value)
+            }
+        }
+    },
+    methods: {
+        getImageUrl
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
