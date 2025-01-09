a64c3f07848ecb4f6b4f2e02c5a19afb706e6745
Mark-Walen <mark_walen@qq.com>
2025-01-09 18:43:21 +0800

    fix: warning

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import path from "path"
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {createSvgIconsPlugin} from "vite-plugin-svg-icons";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -11,6 +11,10 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/blue-bill-admin/',
+    define: {
+        // enable hydration mismatch details in production build
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
+    },
     plugins: [
         vue(),
         createSvgIconsPlugin({
