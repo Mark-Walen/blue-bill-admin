24a664a2f0631032efa73472f4608684a30053d8
Mark-Walen <Walen.Mark2509758@Gmail.com>
2025-01-19 13:49:43 +0000

    Deploying to pro_0.0 from @ Mark-Walen/blue-bill-admin@0378c880fa1547d3520c1d17470262723b8df37b 🚀

---
+++ assets/CreditCard.6dd08ba2.js
@@ -0,0 +1,1 @@
+import{B as o}from"./BmCard.9b07d709.js";import{_ as a,r,o as s,c as n,w as _,R as c}from"./index.1e0e53db.js";const d={name:"CreditCard",components:{BmCard:o}};function p(m,e,i,l,C,f){const t=r("bm-card");return s(),n(t,null,{default:_(()=>e[0]||(e[0]=[c("\u6211\u7684\u94F6\u884C\u5361")])),_:1})}const b=a(d,[["render",p],["__scopeId","data-v-6b3a0c11"]]);export{b as default};
